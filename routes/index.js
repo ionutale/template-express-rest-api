3f5ea5210a565bd113bf9937e627c3c387128676
Ion Utale <utale.ion@gmail.com>
2020-03-17 18:36:39 +0100

    get service status

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/status', function(req, res, next) {
+  try {
+    const memory = process.memoryUsage()
+    const resource = process.resourceUsage()
+    const combined = {
+      memoryUsage: memory,
+      resourceUsage: resource
+    }
+    console.debug(combined)
+    res.status(200).json(combined)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({"err": error.message})
+  }
+})
+
 module.exports = router;
